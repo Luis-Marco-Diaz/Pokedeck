a585e7413afbe273e17f5e2133d6a6b9f0964faa
Marco Diaz <marko.dzm@gmail.com>
2023-05-28 16:47:12 -0600

    ResetMain

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
 import './App.css';
 import axios from 'axios';
-import Pokemon from './Components/Pokemon';
 import { useEffect, useState } from 'react';
 
 
 
 function App() {
-  
-  const [ characterData, setCharacterData ] =
-  useState ({}) 
-
-  useEffect ( () => {
-
-    axios
-    .get("https://pokeapi.co/api/v2/pokemon/charizard")
-    .then( resp => setCharacterData(resp.data) )
-    .catch(error => console.error(error))
-
-  }, [] )
 
   return (
     <div className="App">
-      <Pokemon data={characterData} ></Pokemon>
+     <h1>Hola Mundo</h1>
     </div>
   );
 }
